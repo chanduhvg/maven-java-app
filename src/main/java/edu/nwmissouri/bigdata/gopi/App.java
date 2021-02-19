8b67683e9c3e9066287d78036dc4288888ed2aaa
chanduhvg <chanduhvg@gmail.com>
2021-02-19 14:54:30 -0600

    added pom and src files

---
+++ src/main/java/edu/nwmissouri/bigdata/gopi/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bigdata.gopi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
